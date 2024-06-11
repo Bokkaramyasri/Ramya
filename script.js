8a26fc145c9c27b2f108fb276c7d00a6384279d8
Bokkaramyasri <156409182+Bokkaramyasri@users.noreply.github.com>
2024-06-11 08:06:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('reservation-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Reservation made successfully!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Message sent successfully!');
+});
